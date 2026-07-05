6461540358948d59b0f38188fce3de44c0ea039f
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2026-07-05 19:33:14 +0530

    updated

--- utils/sampling_params.py
+++ utils/sampling_params.py
@@ -4,5 +4,5 @@ from dataclasses import dataclass
 @dataclass(slots=True)
 class SamplingParams:
     temperature: float = 0.0
-    max_tokens: int = 2048
+    max_tokens: int = 128
     ignore_eos: bool = False
